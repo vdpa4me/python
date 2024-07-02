4f78a993a33aabe1f1999cf8c00731016a8f4500
ben chang <ben.chang@neosem.com>
2024-07-02 17:06:52 +0900

    memory_test_parser_prime.py

--- memory_test/memory_test.py
+++ memory_test/memory_test.py
@@ -1,18 +1,99 @@
 #telent connection
 import getpass
 import asyncio
-import telnetlib
+import telnetlib3
 import sys
 import os
 import time
+
+target_time_min = 15
+cmb = ""
+ip = ""
+
+async def main():
+
+    #connect to a CMB
+    print("connected to "+cmb+"("+ip+")")
+
+    port = 23
+
+    reader, writer = await telnetlib3.open_connection(ip, port, encoding='utf-8')
+    print("connected")
+
+    await reader.readuntil(b"login: ")
+    writer.write("root".encode('ascii')+b"\n")
+    await reader.readuntil(b"Password: ")
+    writer.write("a".encode('ascii')+b"\n")
+    print("Logged in")  
+
+    #Memory Test
+    print("1. start mtest (wait 1 minute)")
+    writer.write("mkdir /root/log\n")
+    writer.write("cd /root/log\n")
+    writer.write("rm *\n")
+    writer.write("cd /root/test\n")
+    writer.write("./start_mtest.sh > /root/log/dimm_temp.log &\n")
+    await asyncio.sleep(60)
+
+    #CPU thermal recording
+    #print("2. start ptst")
+    #writer.write("cd /root/ptat\n")
+    #writer.write("cd log\n")
+    #writer.write("rm *\n")
+    #writer.write("cd ..\n")
+    #writer.write("./ptat -log -csv -wf -i 5000000 -t 0 &\n")
+    #await asyncio.sleep(30)
+
+    #wait 15 minutes
+    print("3. wait "+ target_time_min+" minutes")
+    time.sleep(60*target_time_min)
+
+    #end test
+    #print("4. end ptat")
+    #writer.write("pkill -9 -f ptat\n")
+    #await asyncio.sleep(30)
+
+    print("5. end mtest")
+    writer.write("cd /root/test\n")
+    writer.write("pkill -9 -f start_mtest.sh\n")
+    await asyncio.sleep(30)
+    writer.write("./end_mtest.sh\n")
+    await asyncio.sleep(30)
+
+    print("6. set fan speed to 50% (wait 1 minute)")
+    writer.write("/root/fan/fan_control.py --set_fans_pwm 50\n")
+    await asyncio.sleep(60)
+
+    print("7. logging")
+    writer.write("### DIMMTEMP_START ###\n")
+    writer.write("cat /root/log/dimm_temp.log\n")
+    writer.write("### DIMMTEMP_END ###\n")
+    #writer.write("### CPUTEMP_START ###\n")
+    #writer.write("cat /root/ptat/log/*mon*\n")
+    #writer.write("### CPUTEMP_END ###\n")
+    await asyncio.sleep(30)
+
+    dir_name = os.path.dirname(os.path.realpath(__file__))
+    new_file_name = cmb + "_dimm_cpu.log"
+    new_file_name = os.path.join(dir_name, new_file_name)
+    newFile = open(new_file_name, 'w', encoding='utf8')
+    newFile.write(reader.read_all().decode('utf8'))
+  
+    writer.close()
+    reader.close()
+    await writer.wait_closed()
+    await reader.wait_closed()
+
+    print("8. done")
+
+
+
 
 if len(sys.argv) == 1:
     print("argument missing")
     sys.exit(1) 
 
 
-cmb = ""
-ip = ""
 if sys.argv[1] == "1":
     cmb = "CMB1"
     ip = "192.168.0.1"
@@ -49,65 +130,11 @@ elif sys.argv[1] == "11":
 elif sys.argv[1] == "12":
     cmb = "CMB12"
     ip = "192.168.0.12"
+elif sys.argv[1] == "13":
+    cmb = "CMB13"
+    ip = "198.168.0.1"
 else:
     print("invalid argument")
     sys.exit(1)
 
-#connect to a CMB
-print("connected to "+cmb+"("+ip+")")
-tn = telnetlib.Telnet(ip)
-tn.read_until(b"login: ")
-tn.write("root".encode('ascii') + b"\n")
-tn.read_until(b"Password: ")
-tn.write("a".encode('ascii') + b"\n")
-
-#Memory Test 
-print("1. start mtest (wait 1 minute)")
-tn.write(b"mkdir /root/log\n")    
-tn.write(b"cd /root/log\n") 
-tn.write(b"rm *\n")
-tn.write(b"cd /root/test\n")
-tn.write(b"./start_mtest.sh > /root/log/dimm_temp.log &\n")
-time.sleep(60)
-
-#CPU thermal recording 
-print("2. start ptst")
-tn.write(b"cd /root/ptat\n")
-tn.write(b"cd log\n")
-tn.write(b"rm *\n")
-tn.write(b"cd ..\n")
-tn.write(b"./ptat -log -csv -wf -i 5000000 -t 0 &\n")
-time.sleep(30)
-
-print("3. wait 15 minutes")
-time.sleep(60*15)
-print("4. end ptat")
-tn.write(b"pkill -9 -f ptat\n")      
-print("5. end mtest")
-time.sleep(30)
-tn.write(b"cd /root/test\n")
-tn.write(b"pkill -9 -f start_mtest.sh\n")
-time.sleep(30)
-tn.write(b"./end_mtest.sh\n")
-print("6. set fan speed to 50% (wait 1 minute)")
-tn.write(b"/root/fan/fan_control.py --set_fans_pwm 50\n")
-time.sleep(60)
-
-
-print("7. logging")
-tn.write(b"### DIMMTEMP_START ###\n")
-tn.write(b"cat /root/log/dimm_temp.log\n")
-tn.write(b"### DIMMTEMP_END ###\n")
-tn.write(b"### CPUTEMP_START ###\n")
-tn.write(b"cat /root/ptat/log/*mon*\n")  
-tn.write(b"### CPUTEMP_END ###\n")
-
-tn.write(b"exit\n")
-
-dir_name = os.path.dirname(os.path.realpath(__file__))
-new_file_name = cmb + "_dimm_cpu.log"
-new_file_name = os.path.join(dir_name, new_file_name)
-newFile = open(new_file_name, 'w', encoding='utf8')
-newFile.write(tn.read_all().decode('utf8'))
-
-print("8. done")
+asyncio.run(main())
---
+++ memory_test/memory_test_parser_prime.py
@@ -0,0 +1,311 @@
+import sys
+import os
+from os.path import basename
+
+target_iteration = "800/900"
+report_list = []
+
+
+if __name__ == '__main__':
+
+    # get current directory 
+    dir_name = os.path.dirname(os.path.realpath(__file__))
+
+    # get failes in the current directory 
+    file_list = []
+    for (root, directories, files) in os.walk(dir_name):
+        for file in files:
+            file_path = os.path.join(root, file)
+            path,ext = os.path.splitext(file_path)
+            if ext in '.log' :
+                temp_dic = {}
+                # new file for writing
+                new_file_name = file+"_DIMM.csv"
+                new_file_name = os.path.join(dir_name, new_file_name)
+                temp_dic['read'] = file_path
+                temp_dic['write']= new_file_name
+                file_list.append(temp_dic)
+
+    for file_dic in file_list:
+        read_file = file_dic['read']
+        write_file = file_dic['write']
+
+        total_measurment_list = []
+
+        isDimmTest = False
+
+        cmb_num = 1 #default 
+        if "CMB1_" in read_file:
+            cmb_num = 1
+        elif "CMB2_" in read_file:
+            cmb_num = 2
+        elif "CMB3_" in read_file:
+            cmb_num = 3
+        elif "CMB4_" in read_file:
+            cmb_num = 4
+        elif "CMB5_" in read_file:
+            cmb_num = 5
+        elif "CMB6_" in read_file:
+            cmb_num = 6
+        elif "CMB7_" in read_file:
+            cmb_num = 7
+        elif "CMB8_" in read_file:
+            cmb_num = 8
+        elif "CMB9_" in read_file:
+            cmb_num = 9
+        elif "CMB10_" in read_file:
+            cmb_num = 10
+        elif "CMB11_" in read_file:
+            cmb_num = 11
+        elif "CMB12_" in read_file:
+            cmb_num = 12
+
+        # initial read
+        logfile = open(read_file, 'r', encoding='utf8', errors='ignore')
+        current_line = logfile.readline()
+        dut_list = []
+     
+        while current_line:
+            # Skip empty line
+            if "ELAPSE/TOTLDR" in current_line:
+                #ELAPSE/TOTLDR = 20/900
+                iteration_dic = {}
+ 
+                tokens = current_line.split('=')
+                iteration = tokens[1]
+                iteration = iteration.replace('\n','')
+                iteration = iteration.replace(' ','')
+
+                iteration_dic['iteration'] = iteration
+                group1_dic = {}
+                group2_dic = {}
+                group3_dic = {}
+                group4_dic = {}
+
+                g1minVal = 0
+                g1maxVal = 0
+                g2minVal = 0
+                g2maxVal = 0
+                g3minVal = 0
+                g3maxVal = 0
+                g4minVal = 0
+                g4maxVal = 0
+
+                dimm_cnt = 0
+                while(dimm_cnt < 24):
+                    current_line = logfile.readline()
+                    if "CPU0_" not in current_line and "CPU1_" not in current_line:
+                        continue
+                    dimm_cnt += 1
+
+                    if "no reading" in current_line:
+                        DIMMTEMPval = 0
+                    else:
+                        tokens = current_line.split('|')
+                        DIMMNO = tokens[0]
+                        DIMMTEMP = tokens[1]
+                        DIMMTEMP = DIMMTEMP.replace(' degrees C','')                    
+                        DIMMTEMPVal = int(DIMMTEMP)
+
+
+                    cur_grp_no = 0
+
+                  
+                    if "CPU0_DIMM_TEMP_A" in DIMMNO:
+                        group1_dic['A'] = DIMMTEMPVal
+                        cur_grp_no = 1
+                    elif "CPU0_DIMM_TEMP_B" in DIMMNO:
+                        group1_dic['B'] = DIMMTEMPVal
+                        cur_grp_no = 1
+                    elif "CPU0_DIMM_TEMP_C" in DIMMNO:
+                        group1_dic['C'] = DIMMTEMPVal
+                        cur_grp_no = 1
+                    elif "CPU0_DIMM_TEMP_D" in DIMMNO:
+                        group1_dic['D'] = DIMMTEMPVal
+                        cur_grp_no = 1
+                    elif "CPU0_DIMM_TEMP_E" in DIMMNO:
+                        group1_dic['E'] = DIMMTEMPVal
+                        cur_grp_no = 1
+                    elif "CPU0_DIMM_TEMP_F" in DIMMNO:
+                        group1_dic['F'] = DIMMTEMPVal
+                        cur_grp_no = 1
+
+                    elif "CPU0_DIMM_TEMP_G" in DIMMNO:
+                        group2_dic['G'] = DIMMTEMPVal
+                        cur_grp_no = 2
+                    elif "CPU0_DIMM_TEMP_H" in DIMMNO:
+                        group2_dic['H'] = DIMMTEMPVal
+                        cur_grp_no = 2
+                    elif "CPU0_DIMM_TEMP_I" in DIMMNO:
+                        group2_dic['I'] = DIMMTEMPVal
+                        cur_grp_no = 2
+                    elif "CPU0_DIMM_TEMP_J" in DIMMNO:
+                        group2_dic['J'] = DIMMTEMPVal
+                        cur_grp_no = 2
+                    elif "CPU0_DIMM_TEMP_K" in DIMMNO:
+                        group2_dic['K'] = DIMMTEMPVal
+                        cur_grp_no = 2
+                    elif "CPU0_DIMM_TEMP_L" in DIMMNO:
+                        group2_dic['L'] = DIMMTEMPVal
+                        cur_grp_no = 2
+
+                    elif "CPU1_DIMM_TEMP_A" in DIMMNO:
+                        group3_dic['A'] = DIMMTEMPVal
+                        cur_grp_no = 3
+                    elif "CPU1_DIMM_TEMP_B" in DIMMNO:
+                        group3_dic['B'] = DIMMTEMPVal
+                        cur_grp_no = 3
+                    elif "CPU1_DIMM_TEMP_C" in DIMMNO:
+                        group3_dic['C'] = DIMMTEMPVal
+                        cur_grp_no = 3
+                    elif "CPU1_DIMM_TEMP_D" in DIMMNO:
+                        group3_dic['D'] = DIMMTEMPVal
+                        cur_grp_no = 3
+                    elif "CPU1_DIMM_TEMP_E" in DIMMNO:
+                        group3_dic['E'] = DIMMTEMPVal
+                        cur_grp_no = 3
+                    elif "CPU1_DIMM_TEMP_F" in DIMMNO:
+                        group3_dic['F'] = DIMMTEMPVal
+                        cur_grp_no = 3
+
+                    elif "CPU1_DIMM_TEMP_G" in DIMMNO:
+                        group4_dic['G'] = DIMMTEMPVal
+                        cur_grp_no = 4
+                    elif "CPU1_DIMM_TEMP_H" in DIMMNO:
+                        group4_dic['H'] = DIMMTEMPVal
+                        cur_grp_no = 4
+                    elif "CPU1_DIMM_TEMP_I" in DIMMNO:
+                        group4_dic['I'] = DIMMTEMPVal
+                        cur_grp_no = 4
+                    elif "CPU1_DIMM_TEMP_J" in DIMMNO:
+                        group4_dic['J'] = DIMMTEMPVal
+                        cur_grp_no = 4
+                    elif "CPU1_DIMM_TEMP_K" in DIMMNO:
+                        group4_dic['K'] = DIMMTEMPVal
+                        cur_grp_no = 4
+                    elif "CPU1_DIMM_TEMP_L" in DIMMNO:
+                        group4_dic['L'] = DIMMTEMPVal
+                        cur_grp_no = 4
+                
+                    if(cur_grp_no == 1):
+                        if g1minVal == 0:
+                            g1minVal = DIMMTEMPVal
+                            g1maxVal = DIMMTEMPVal
+                        else:
+                            if DIMMTEMPVal < g1minVal:
+                                g1minVal = DIMMTEMPVal
+                            if DIMMTEMPVal > g1maxVal:
+                                g1maxVal = DIMMTEMPVal
+                    elif(cur_grp_no == 2):
+                        if g2minVal == 0:
+                            g2minVal = DIMMTEMPVal
+                            g2maxVal = DIMMTEMPVal
+                        else:
+                            if DIMMTEMPVal < g2minVal:
+                                g2minVal = DIMMTEMPVal
+                            if DIMMTEMPVal > g2maxVal:
+                                g2maxVal = DIMMTEMPVal
+                    elif(cur_grp_no == 3):
+                        if g3minVal == 0:
+                            g3minVal = DIMMTEMPVal
+                            g3maxVal = DIMMTEMPVal
+                        else:
+                            if DIMMTEMPVal < g3minVal:
+                                g3minVal = DIMMTEMPVal
+                            if DIMMTEMPVal > g3maxVal:
+                                g3maxVal = DIMMTEMPVal
+                    elif(cur_grp_no == 4):
+                        if g4minVal == 0:
+                            g4minVal = DIMMTEMPVal
+                            g4maxVal = DIMMTEMPVal
+                        else:
+                            if DIMMTEMPVal < g4minVal:
+                                g4minVal = DIMMTEMPVal
+                            if DIMMTEMPVal > g4maxVal:
+                                g4maxVal = DIMMTEMPVal
+                    else:
+                        print("ERROR: " + DIMMNO)
+                        exit(1)
+
+                group1_dic['MIN'] = g1minVal
+                group1_dic['MAX'] = g1maxVal
+                group1_dic['DIFF'] = g1maxVal - g1minVal
+
+                group2_dic['MIN'] = g2minVal
+                group2_dic['MAX'] = g2maxVal
+                group2_dic['DIFF'] = g2maxVal - g2minVal
+
+                group3_dic['MIN'] = g3minVal
+                group3_dic['MAX'] = g3maxVal
+                group3_dic['DIFF'] = g3maxVal - g3minVal
+
+                group4_dic['MIN'] = g4minVal
+                group4_dic['MAX'] = g4maxVal
+                group4_dic['DIFF'] = g4maxVal - g4minVal
+
+                iteration_dic['g1'] = group1_dic
+                iteration_dic['g2'] = group2_dic
+                iteration_dic['g3'] = group3_dic
+                iteration_dic['g4'] = group4_dic
+
+                total_measurment_list.append(iteration_dic)
+
+                if target_iteration in iteration:
+                    print("Target iteration found: " + target_iteration)
+                    report_dic = {}
+                    report_dic['CMB'] = cmb_num
+                    report_dic['value'] = iteration_dic
+                    report_list.append(report_dic)
+                    
+            current_line = logfile.readline()
+        logfile.close()
+
+        # write to csv
+        with open(write_file, 'w') as f:
+            f.write("##############################################\n")
+            f.write("1. DIMM Temperature Uniformity \n")
+            f.write("##############################################\n")
+            f.write("\n")
+            f.write("Iteration,CPU0_F,CPU0_E,CPU0_D,CPU0_C,CPU0_B,CPU0_A,G1_MIN,G1_MAX,G1_DIFF,\
+                              ,CPU0_G,CPU0_H,CPU0_I,CPU0_J,CPU0_K,CPU0_L,G2_MIN,G2_MAX,G2_DIFF \n")
+                       
+            for i in range(0, len(total_measurment_list)):
+                iteration = total_measurment_list[i]['iteration'] 
+                group1_dic = total_measurment_list[i]['g1']
+                group2_dic = total_measurment_list[i]['g2']
+                #group3_dic = total_measurment_list[i]['g3']
+                #group4_dic = total_measurment_list[i]['g4']
+
+                f.write(iteration+","+str(group1_dic['F'])+","+str(group1_dic['E'])+","+str(group1_dic['D'])+","+str(group1_dic['C'])+","+str(group1_dic['B'])+","+str(group1_dic['A'])+ \
+                                  ","+str(group1_dic['MIN'])+","+str(group1_dic['MAX'])+","+str(group1_dic['DIFF'])+","+ \
+                                  ","+str(group2_dic['G'])+","+str(group2_dic['H'])+","+str(group2_dic['I'])+","+str(group2_dic['J'])+","+str(group2_dic['K'])+","+str(group2_dic['L'])+ \
+                                  ","+str(group2_dic['MIN'])+","+str(group2_dic['MAX'])+","+str(group2_dic['DIFF'])+"\n")
+            f.write("\n")
+            f.close()
+    
+
+    # wirte to report
+    with open("TotalReport.csv", 'w') as rf:
+        rf.write("##############################################\n")
+        rf.write("1. DIMM Temperature Uniformity \n")
+        rf.write("##############################################\n")
+        rf.write("\n")
+        rf.write("CMB,Iteration,CPU0_F,CPU0_E,CPU0_D,CPU0_C,CPU0_B,CPU0_A,G1_MIN,G1_MAX,G1_DIFF,\
+                               ,CPU0_G,CPU0_H,CPU0_I,CPU0_J,CPU0_K,CPU0_L,G2_MIN,G2_MAX,G2_DIFF \n")
+        report_list.sort(key=lambda x: x['CMB'], reverse=True)
+        for i in range(0, len(report_list)):
+            cmb_num = report_list[i]['CMB']
+            iteration = report_list[i]['value']['iteration'] 
+            group1_dic = report_list[i]['value']['g1']
+            group2_dic = report_list[i]['value']['g2']
+            #group3_dic = report_list[i]['value']['g3']
+            #group4_dic = report_list[i]['value']['g4']
+
+            rf.write(str(cmb_num)+","+iteration+","+str(group1_dic['F'])+","+str(group1_dic['E'])+","+str(group1_dic['D'])+","+str(group1_dic['C'])+","+str(group1_dic['B'])+","+str(group1_dic['A'])+ \
+                                ","+str(group1_dic['MIN'])+","+str(group1_dic['MAX'])+","+str(group1_dic['DIFF'])+","+ \
+                                ","+str(group2_dic['G'])+","+str(group2_dic['H'])+","+str(group2_dic['I'])+","+str(group2_dic['J'])+","+str(group2_dic['K'])+","+str(group2_dic['L'])+ \
+                                ","+str(group2_dic['MIN'])+","+str(group2_dic['MAX'])+","+str(group2_dic['DIFF'])+"\n")
+        rf.close()
+    
+
+
